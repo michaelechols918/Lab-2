627ae810ee43157470608ebf9fa3890432cd8937
Yoshishimi <michaelechols918@yahoo.com>
2016-03-16 12:05:19 -0700

    Done with app.js

---
+++ app.js
@@ -0,0 +1,92 @@
+console.log('Welcome to my portfolio ^___^');
+alert('Oh, hello there.');
+
+var userName = prompt('Before we begin, what is your name?')
+  console.log('enter your name');
+  alert('Hello, ' + userName + '!');
+
+var readyForThis = prompt('Are yall ready for this? Yes or No');
+if (readyForThis.toLowerCase() ==='yes'){
+  console.log('Yes')
+  alert('Correct, ' + userName + '!! NAH NAH NAH NAHNAHNAHNUHUHUHUHUHUHUHUH!!!')
+}
+else if (readyForThis.toLowerCase() ==='no'){
+  console.log('No')
+  alert('Incorrect, ' + userName + '! Those Feels, Man. Those Feels.')
+}
+else {
+  console.log('anything else')
+  alert('Yes or No, ' + userName + '!!! WAKE UP (WAKE UP) WHY DONT YOU PUT ON A LIL MAKEUP!!')
+}
+
+var pokeMon = prompt('Do I look like a Pokemon Master?');
+if (pokeMon.toLowerCase() ==='yes'){
+  console.log('Yes');
+  alert('Correct, ' + userName + '!! I WANNA BE THE VERY BEST THAT NO ONE EVER WAS!!!!!!!.');
+}
+else if (pokeMon.toLowerCase() ==='no'){
+  console.log('No')
+  alert('Incorrect, ' + userName + '!! Well, I did beat the Elite 4 with just Magikarp.');
+}
+else {
+  console.log('anything else');
+  alert('Yes or No ' + userName + '!!(Uses Full Restore) Did you get hit by a Confuse Ray from Zubat?');
+}
+var thorsHammer = prompt('Am I worthy enough to hold The Hammer of Thor? Yes or No.');
+// check for yes
+if (thorsHammer.toLowerCase() ==='yes') {
+  // do somehting
+  console.log('Yes');
+  alert('Correct, ' + userName + '!! Thank You Odinson, I shall fight for Asguard...WITH HONOR!!!!');
+  // check for no
+}
+else  if (thorsHammer.toLowerCase() ==='no'){
+  // do somehting else
+  console.log('No');
+  alert('Incorrect,' + userName + '!! How Dare Thee! Prepare...FOR MY THUNDER!!!!!!');
+}
+else{
+console.log('anything else')
+alert('Yes or No ' + userName + '!! Did Loki inflict a curse upon yourself? Yes or No, Mortal!!');
+}
+var kungFu = prompt('Can my Kung Fu skills come in handy during coding? Yes or No.');
+if (kungFu.toLowerCase() ==='yes'){
+  console.log('Yes');
+  alert('Correct, ' + userName + '!! Thank you for noticing me, Senpai!!');
+}
+else if (thorsHammer.toLowerCase() ==='no'){
+  console.log('No');
+  alert('Incorrect, ' + userName + '!! Well I guess I need to demonstrate my Drunken Tiger Fist to you.');
+}
+else {
+  console.log('anything else')
+  alert('Focus, ' + userName + '!!!!! Yes or No. Wax on. Wax off.');
+}
+var shibaInu = prompt('Did you know I have a cute Shiba Inu named Yoshi?');
+if (shibaInu.toLowerCase() ==='yes'){
+  console.log('Yes');
+  alert('Correct, ' + userName + '!! Tehehe, You must be psychic. no more questions! :D');
+}
+else if (shibaInu.toLowerCase() ==='no'){
+  console.log('No');
+  alert('Incorrect, ' + userName + '!! Really? You should meet him sometime. He loves hugs and kisses from everyone!');
+}
+else {
+  console.log('anything else');
+  alert('Yes or No, ' + userName + '!! Bark Bark Bark!!! aka "Yes or No!"');
+}
+
+if (shibaInu.toLowerCase() ==='no'){
+  console.log('no')
+  var annoyingPopUp = prompt('Do you think this is the last add now? Yes or No.');
+  if (annoyingPopUp.toLowerCase() ==='yes'){
+  console.log('Yes');
+  alert('Correct, ' + userName + '!! Are you Prof. X?? You\'re bald and psychic to the max!!')}
+  else if (annoyingPopUp.toLowerCase() ==='no') {
+  console.log('no')
+  alert('Incorrect, ' + userName + '!! Aww. Come on! Have some hope in humanity!')}
+  else {
+    console.log('anything else')
+    alert('Yes or No, userName! I am SUPER CEREAL right now!')
+  }
+}
